dd7e9d0959f4b5d0e0e47560e6beab546bb390e9
Cheng-Ta Chu <c.chu15@imperial.ac.uk>
2017-12-14 20:35:15 +0000

    update

--- model_VI_plan.py
+++ model_VI_plan.py
@@ -66,7 +66,7 @@ def calInvestmentPlanning(objMarket, instance):
 
                 # calculate the LCOE of renewables
 
-                # install the new plant with lowest LCOE (add to objSubregion.listGenPlantFuture and objSubregion.listGenPlantOperTemp)
+                # install the new plant with lowest LCOE (add to objZone.listGenPlantFuture and objZone.listGenPlantOperTemp)
 
                 # re-dispatch
 
@@ -88,7 +88,7 @@ def calInvestmentPlanning(objMarket, instance):
                 # calculate the LCOE of all plant
                 calAllNewPlantLCOE(instance, objMarket, iYearStep)
 
-                # install the new plant with lowest LCOE (add to objSubregion.listGenPlantFuture and objSubregion.listGenPlantOperTemp)
+                # install the new plant with lowest LCOE (add to objZone.listGenPlantFuture and objZone.listGenPlantOperTemp)
 
 
                 # re-dispatch
@@ -128,45 +128,48 @@ def calAllNewPlantLCOE(instance, objMarket, indexYear):
                 # reach capacity limit
                 objNewProcessCandidate.fLCOE = 9999
 
-            elif objNewProcessCandidate.sOperationMode == "Dispatch" or objNewProcessCandidate.sOperationMode == "NonDispatch":
+            elif objNewProcessCandidate.sOperationMode in ["Dispatch", "NonDispatch", "LimitDispatch"]:
 
                 if objNewProcessCandidate.sOperationMode == "NonDispatch":
                     model_util_gen.calNonDispatchGeneration(instance, objZone, objNewProcessCandidate, indexYear)
-
+                elif objNewProcessCandidate.sOperationMode == "LimitDispatch":
+                    model_util_gen.calLimitedDispatchGeneration(instance, objZone, objNewProcessCandidate, indexYear)
+                    
                 fPlantTotalGeneration = 0   # MW
-                for indexTS, objTimeSlice in enumerate(instance.listTimeSlice):
+                for indexTS, objTimeSlice in enumerate(instance.lsTimeSlice): 
 
-                    fMaxGeneration = objNewPlantCandidate.fDeratedCapacity  # MW
-                    if objNewPlantCandidate.sOperationMode == "NonDispatch":
-                        fMaxGeneration = objNewPlantCandidate.fHourNetGeneration_TS_YR[indexTS, indexYear]
+                    fMaxGeneration = objNewProcessCandidate.fDeratedCapacity  # MW
+                    if objNewProcessCandidate.sOperationMode in ["NonDispatch","LimitDispatch"]:
+                        fMaxGeneration = objNewProcessCandidate.fHourlyPowerOutput_TS_YS[indexTS, indexYear]
 
-                    if fMaxGeneration <= objZone.aResidualDemand_TS_YR[indexTS, indexYear]:
+                    if fMaxGeneration <= objZone.fPowerResDemand_TS_YS[indexTS, indexYear]:
                         # full dispatch
-                        fPlantTotalGeneration += fMaxGeneration * objTimeSlice.RepresentHours    # MWh
+                        fPlantTotalGeneration += fMaxGeneration * objTimeSlice.iRepHoursInYear    # MWh
                     else:
                         # partially local dispatch
-                        fPlantTotalGeneration += objZone.aResidualDemand_TS_YR[indexTS, indexYear] * objTimeSlice.RepresentHours
-                        fMaxGeneration = fMaxGeneration - objZone.aResidualDemand_TS_YR[indexTS, indexYear]
+                        fPlantTotalGeneration += objZone.fPowerResDemand_TS_YS[indexTS, indexYear] * objTimeSlice.iRepHoursInYear
+                        fMaxGeneration = fMaxGeneration - objZone.fPowerResDemand_TS_YS[indexTS, indexYear]
 
                         # calculate max export to all neighbor subregion
                         fMaxExportTS = 0
-                        for iPathIndex, objConnPath in enumerate(objZone.listConnectPath): 
-                            if objConnPath.fAvailTransCapacity[indexTS] > 0:
-                                fDestSubregionNodalPrice = instance.listSubregionObjs[objConnPath.iDestSubregionIndex].aNodalPrice_TS_YR[indexTS, indexYear]
-                                if fDestSubregionNodalPrice > objNewPlantCandidate.fGenCostPerUnit_YR[indexYear]:
-                                    fMaxExportTS += objConnPath.fAvailTransCapacity[indexTS]
+                        for iPathIndex, objConnPath in enumerate(objZone.lsConnectPath): 
+                            if objConnPath.fAvailTransCapacity_TS[indexTS] > 0:
+                                fDestSubregionNodalPrice = instance.lsZone[objConnPath.iDestZoneIndex].fNodalPrice_TS_YS[indexTS, indexYear]
+                                if fDestSubregionNodalPrice > objNewProcessCandidate.fVariableGenCost_TS_YS[indexYear]:
+                                    fMaxExportTS += objConnPath.fAvailTransCapacity_TS[indexTS]
 
                         fExport = min(fMaxExportTS, fMaxGeneration)
-                        fPlantTotalGeneration += fExport * objTimeSlice.RepresentHours    # MWh
+                        fPlantTotalGeneration += fExport * objTimeSlice.iRepHoursInYear    # MWh
                         
                         if fMaxGeneration > 0:
-                            if objNewPlantCandidate.fGenCostPerUnit_YR[indexYear] < objZone.aNodalPrice_TS_YR[indexTS, indexYear]:
-                                fPlantTotalGeneration += fMaxGeneration * objTimeSlice.RepresentHours    # MWh
-                        
+                            if objNewProcessCandidate.fVariableGenCost_TS_YS[indexYear] < objZone.fNodalPrice_TS_YS[indexTS, indexYear]:
+                                fPlantTotalGeneration += fMaxGeneration * objTimeSlice.iRepHoursInYear    # MWh
+                                
+                #-------------------------------------------------------
                 # annual fixed investment (MillionUSD / year)
-                fNewPlantCost = objNewPlantCandidate.fAnnualFixedCost
+                fNewPlantCost = objNewProcessCandidate.fAnnualFixedCost
                 # total generation cost = total generation (MWh) *  unit generation cost (USD/kWh) / 1000 = M.USD
-                fNewPlantCost += fPlantTotalGeneration * objNewPlantCandidate.fGenCostPerUnit_YR[indexYear] / 1000
+                fNewPlantCost += fPlantTotalGeneration * objNewProcessCandidate.fVariableGenCost_TS_YS[indexYear] / 1000
 
                 # LCOE = M.USD / MWh * 1000 = USD/kWh
                 if fPlantTotalGeneration < 1:
@@ -175,18 +178,18 @@ def calAllNewPlantLCOE(instance, objMarket, indexYear):
                     fLCOE = fNewPlantCost / fPlantTotalGeneration * 1000
 
                     # take into account the back-up cost of non-dispatchable plant
-                    if objNewPlantCandidate.sOperationMode == "NonDispatch":
+                    if objNewProcessCandidate.sOperationMode == "NonDispatch":
                         # calculate the change of the disparity of peak and flat demand after install the new non-dispatchable plant
                         # find peak and flat demand
-                        indexPeakDemand = np.argmax(objZone.aResidualDemand_TS_YR[:, indexYear])
-                        indexFlatDemand = np.argmin(objZone.aResidualDemand_TS_YR[:, indexYear])
-                        iPeakOutput = objNewPlantCandidate.fHourNetGeneration_TS_YR[indexPeakDemand,indexYear]
-                        iFlatOutput = objNewPlantCandidate.fHourNetGeneration_TS_YR[indexFlatDemand,indexYear]
-                        fPositiveDisparity = iPeakOutput - iFlatOutput  # MW
+                        indexPeakDemand = np.argmax(objZone.fPowerResDemand_TS_YS[:, indexYear])
+                        indexValleyDemand = np.argmin(objZone.fPowerResDemand_TS_YS[:, indexYear])
+                        iPeakOutput = objNewProcessCandidate.fHourlyPowerOutput_TS_YS[indexPeakDemand,indexYear]
+                        iValleyOutput = objNewProcessCandidate.fHourlyPowerOutput_TS_YS[indexValleyDemand,indexYear]
+                        fPositiveDisparity = iPeakOutput - iValleyOutput  # MW
                         if fPositiveDisparity < 0:
                             # get the cheapest back-up plant cost
                             fBackUpCost = 99999
-                            for indexP, objNewPlant in enumerate(objZone.listNewPlantCandidate):
+                            for indexP, objNewPlant in enumerate(objZone.lsNewProcessCandidate):
                                 if objNewPlant.sOperationMode == "Dispatch" and objNewPlant.fAnnualFixedCostPerMW < fBackUpCost:
                                     fBackUpCost = objNewPlant.fAnnualFixedCostPerMW
                             # USD/KW * MW * 1000 = USD
@@ -194,20 +197,40 @@ def calAllNewPlantLCOE(instance, objMarket, indexYear):
                             # USD / MWh / 1000 = USD/kWh
                             fLCOE += fBackUpCost / fPlantTotalGeneration / 1000
 
-                objNewPlantCandidate.fLCOE = fLCOE
+                objNewProcessCandidate.fLCOE = fLCOE
 
-            elif objNewPlantCandidate.sOperationMode == "Storage":
+            elif objNewProcessCandidate.sOperationMode == "Storage":
                 # consider new storage only when residual demand is 0 in more than half of the TS
                 iZeroDemandTSCount = 0
-                for indexTS, objTimeSlice in enumerate(instance.listTimeSlice):
-                    if objZone.aResidualDemand_TS_YR[indexTS, indexYear] < 1:
+                for indexTS, objTimeSlice in enumerate(instance.lsTimeSlice): 
+                    if objZone.fPowerResDemand_TS_YS[indexTS, indexYear] < 1:
                         iZeroDemandTSCount = iZeroDemandTSCount + 1
-                if iZeroDemandTSCount <= len(instance.listTimeSlice) / 2 or iZeroDemandTSCount == len(instance.listTimeSlice): 
-                    objNewPlantCandidate.fLCOE = 9999
+                if iZeroDemandTSCount <= len(instance.lsTimeSlice) / 2 or iZeroDemandTSCount == len(instance.lsTimeSlice): 
+                    objNewProcessCandidate.fLCOE = 9999
                 else:
-                    objNewPlantCandidate.fLCOE = _calHPSOperationLCOE(instance, objMarket, objSubregion, objNewPlantCandidate, indexYear)
+                    objNewProcessCandidate.fLCOE = _calHPSOperationLCOE(instance, objMarket, objZone, objNewProcessCandidate, indexYear)
 
     return
+
+
+
+def _calHPSOperationLCOE(instance, objMarket, objZone, objProcess, indexYear):
+    ''' calculate hydro-pump storage operation and LCOE '''
+
+    model_util_gen.calHPSOperation(instance, objZone, objProcess, indexYear)
+
+    # calculate LCOE
+    fOperationProfit = 0
+    for indexTS, objTimeSlice in enumerate(instance.lsTimeSlice): 
+        # total generation (MWh) *  unit generation cost (USD/kWh) / 1000 = M.USD
+        fOperationProfit += objProcess.fHourlyPowerOutput_TS_YS[indexTS, indexYear] * objTimeSlice.iRepHoursInYear * \
+        objZone.fNodalPrice_TS_YS[indexTS,indexYear] / 1000
+
+    fLCOE = objProcess.fAnnualFixedCost + fOperationProfit
+    fLCOE = 9999 if fLCOE < 0 else fLCOE
+
+    return fLCOE
+
 
 
 
